3136b6a59e72cd76071487af367ac88218c69027
David <david.cabello.info@gmail.com>
2020-11-26 22:59:38 +0100

    implementacion InterfaceAvanzar

--- app/src/main/java/es/studium/myavatar/InterfaceAvanzar.java
+++ app/src/main/java/es/studium/myavatar/InterfaceAvanzar.java
@@ -1,4 +1,23 @@
 package es.studium.myavatar;
 
 public interface InterfaceAvanzar {
+
+    //Métodos para manipular los Diálogos
+    public void aceptarDialogo();
+    public void cancelarDialogo();
+    public void seleccionarDialogo();
+
+    //Métodos para guardar información
+    public void setDatosDialogoNombre(String nombre);
+    public void setDatosDialogoEspecie(String especie);
+    public void setDatosDialogoSexo(String sexo);
+    public void setDatosDialogoProfesion(String profesion);
+
+    //Métodos para ejecutar los diálogos
+    public void ejecutarSegundoDialogo();
+    public void ejecutarTercerDialogo();
+    public void ejecutarCuartoDialogo();
+
+    //Función Random
+    public void randomStats();
 }
