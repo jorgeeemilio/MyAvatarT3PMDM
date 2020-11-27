c286e288ab7081704e693f3831f061ce488624c5
David <david.cabello.info@gmail.com>
2020-11-27 10:35:39 +0100

    implementando vector image  dialogoNombre y metodo random

--- app/src/main/java/es/studium/myavatar/DialogoNombreAvatar.java
+++ app/src/main/java/es/studium/myavatar/DialogoNombreAvatar.java
@@ -1,4 +1,29 @@
 package es.studium.myavatar;
 
-public class DialogoNombreAvatar {
+import android.app.AlertDialog;
+import android.app.Dialog;
+import android.content.DialogInterface;
+import android.os.Bundle;
+
+import androidx.fragment.app.DialogFragment;
+
+public class DialogoNombreAvatar extends DialogFragment {
+    @Override
+    public Dialog onCreateDialog(Bundle savedInstanceState) {
+        // Use the Builder class for convenient dialog construction
+        AlertDialog.Builder builder = new AlertDialog.Builder(getActivity());
+        builder.setMessage(R.string.txt_dialogNombreAvatar)
+                .setPositiveButton(R.string.txt_btnAceptarDialog, new DialogInterface.OnClickListener() {
+                    public void onClick(DialogInterface dialog, int id) {
+                        // FIRE ZE MISSILES!
+                    }
+                })
+                .setNegativeButton(R.string.txt_btnCancelarDialog, new DialogInterface.OnClickListener() {
+                    public void onClick(DialogInterface dialog, int id) {
+                        // User cancelled the dialog
+                    }
+                });
+        // Create the AlertDialog object and return it
+        return builder.create();
+    }
 }
--- app/src/main/java/es/studium/myavatar/MainActivity.java
+++ app/src/main/java/es/studium/myavatar/MainActivity.java
@@ -3,12 +3,83 @@ package es.studium.myavatar;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.EditText;
 
-public class MainActivity extends AppCompatActivity {
+import java.util.Random;
+
+public class MainActivity extends AppCompatActivity implements InterfaceAvanzar{
+    Random rand = new Random();
+    EditText campoTextVida;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        campoTextVida = findViewById(R.id.editTextNumberVida);
+    }
+
+    @Override
+    public void aceptarDialogo() {
+
+    }
+
+    @Override
+    public void cancelarDialogo() {
+
+    }
+
+    @Override
+    public void seleccionarDialogo() {
+
+    }
+
+    @Override
+    public void setDatosDialogoNombre(String nombre) {
+
+    }
+
+    @Override
+    public void setDatosDialogoEspecie(String especie) {
+
+    }
+
+    @Override
+    public void setDatosDialogoSexo(String sexo) {
+
+    }
+
+    @Override
+    public void setDatosDialogoProfesion(String profesion) {
+
+    }
+
+    @Override
+    public void ejecutarSegundoDialogo() {
+
+    }
+
+    @Override
+    public void ejecutarTercerDialogo() {
+
+    }
+
+    @Override
+    public void ejecutarCuartoDialogo() {
+
+    }
+
+    @Override
+    public void randomStats() {
+
+
+        int vida = rand.nextInt(100 - 1)+1;
+        campoTextVida.setText("   "+vida+" puntos de HP");
+        int magia = rand.nextInt(10 - 1)+1;
+        campoTextVida.setText("   "+magia+" puntos de MG");
+        int fuerza = rand.nextInt(20 - 1)+1;
+        campoTextVida.setText("   "+fuerza+" puntos de PW");
+        int velocidad = rand.nextInt(5 - 1)+1;
+        campoTextVida.setText("   "+velocidad+" puntos de SP");
     }
 }
