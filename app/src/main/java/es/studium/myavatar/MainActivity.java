223748f24e9f58d047d5bbb774050b269c578c95
David <david.cabello.info@gmail.com>
2020-11-27 11:31:31 +0100

    añadiendo funcionalidad boton crear y dialogoNombre

--- app/src/main/java/es/studium/myavatar/MainActivity.java
+++ app/src/main/java/es/studium/myavatar/MainActivity.java
@@ -2,21 +2,51 @@ package es.studium.myavatar;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+
+import android.app.Dialog;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 import android.widget.EditText;
 
 import java.util.Random;
 
-public class MainActivity extends AppCompatActivity implements InterfaceAvanzar{
+public class MainActivity extends AppCompatActivity implements InterfaceAvanzar, View.OnClickListener{
     Random rand = new Random();
-    EditText campoTextVida;
+
+    private Button btn_crear;
+    private EditText campoTextVida;
+    private EditText campoTextMagia;
+    private EditText campoTextFuerza;
+    private EditText campoTextVelocidad;
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        btn_crear = findViewById(R.id.btn_crearAvatar);
+        btn_crear.setOnClickListener(this);
         campoTextVida = findViewById(R.id.editTextNumberVida);
+        campoTextMagia = findViewById(R.id.editTextNumberMagia);
+        campoTextFuerza = findViewById(R.id.editTextNumberFuerza);
+        campoTextVelocidad = findViewById(R.id.editTextNumberVelocidad);
+
+    }
+    public void clickCrear (View v){
+
+        DialogoNombreAvatar nombreDialogo = new DialogoNombreAvatar();
+        nombreDialogo.setCancelable(false);
+        nombreDialogo.show(getSupportFragmentManager(), "Diálogo Nombre");
+
+    }
+    @Override
+    public void onClick(View v) {
+        if(v.equals(btn_crear)){
+
+}
     }
 
     @Override
@@ -72,14 +102,15 @@ public class MainActivity extends AppCompatActivity implements InterfaceAvanzar{
     @Override
     public void randomStats() {
 
-
         int vida = rand.nextInt(100 - 1)+1;
         campoTextVida.setText("   "+vida+" puntos de HP");
         int magia = rand.nextInt(10 - 1)+1;
-        campoTextVida.setText("   "+magia+" puntos de MG");
+        campoTextMagia.setText("   "+magia+" puntos de MG");
         int fuerza = rand.nextInt(20 - 1)+1;
-        campoTextVida.setText("   "+fuerza+" puntos de PW");
+        campoTextFuerza.setText("   "+fuerza+" puntos de PW");
         int velocidad = rand.nextInt(5 - 1)+1;
-        campoTextVida.setText("   "+velocidad+" puntos de SP");
+        campoTextVelocidad.setText("   "+velocidad+" puntos de SP");
     }
+
+
 }
